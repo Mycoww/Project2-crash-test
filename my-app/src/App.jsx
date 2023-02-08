220853a8c89c7639fa9b9b917cd83733bae1d046
Thomas <119404291+Thomas-Wcs@users.noreply.github.com>
2023-02-08 15:56:56 +0100

    s1 etape1 question-Questionnaire

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
+import Questionnaire from './components/question/question';
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <Questionnaire />
+    </div>
+  );
 }
 
 export default App;
