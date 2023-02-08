7e40d881a99fdd4027093edb847dce3049284203
macaron23 <laurence.ciancia@gmail.com>
2023-02-08 16:46:20 +0100

    header added

---
+++ my-app/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = (props) => {
+  return (
+    <header className="home-header"></header>
+    <h1 className='home-main-title'>
+      KESQU'onregarde ?
+    </h1>
+
+  )
+}
+
+export default Header
