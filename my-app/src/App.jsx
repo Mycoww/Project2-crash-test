1461bd873269bb2268aced93ae43df478e4457cc
macaron23 <laurence.ciancia@gmail.com>
2023-02-09 15:21:20 +0100

    S01_Header finished

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Footer from './components/footer/Footer';
 import Header from './components/header/Header';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <Footer />
     </div>
   );
 }
